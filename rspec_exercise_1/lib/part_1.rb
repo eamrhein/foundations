8bd5abb5ec9b30a092d2ac3b01d2946957db3fad
Eric Amrhein <eamrhein@protonmail.ch>
2019-06-17 00:10:07 -0700

    rspec_exercise_1/ refactoring

--- rspec_exercise_1/lib/part_1.rb
+++ rspec_exercise_1/lib/part_1.rb
@@ -1,31 +1,29 @@
+# frozen_string_literal: true
+
 def average(num1, num2)
-            (num1 + num2).to_f / 2
+  (num1 + num2).to_f / 2
 end
 
 def average_array(array)
-        array.sum.to_f / array.length
+  array.sum.to_f / array.length
 end
 
 def repeat(str, num)
-    result = ""
-    num.times do 
-        result += str
-    end
-    result
+  result = ''
+  num.times do
+    result += str
+  end
+  result
 end
 
-def yell (str)
-    str.upcase! + "!"
+def yell(str)
+  str.upcase! + '!'
 end
 
 def alternating_case(str)
-    words = str.split
-    words.each_with_index do |el, i|
-        if i.odd?
-            el.downcase!
-        else
-            el.upcase!
-        end
-    end
-    words.join(" ")
+  words = str.split
+  words.each_with_index do |el, i|
+    i.odd? ? el.downcase! : el.upcase!
+  end
+  words.join(' ')
 end
