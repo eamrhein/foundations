e8eb130fb74719dcb1c2db867a93e7472ddbda89
Eric Amrhein <eamrhein@protonmail.ch>
2019-06-16 18:20:37 -0700

    Improved Monkey Patching

--- monkey_patching_project/lib/array.rb
+++ monkey_patching_project/lib/array.rb
@@ -1,63 +1,57 @@
+# frozen_string_literal: true
+
 # Monkey-Patch Ruby's existing Array class to add your own custom methods
 class Array
   def span
-    if self.length == 0
-      return nil
-    end
-    self.max - self.min
+    return nil if length.zero?
+
+    max - min
   end
+
   def average
-    if self.length ==0
-      return nil
-    end
-    self.sum.to_f / self.length
+    return nil if length.zero?
+
+    sum.to_f / length
   end
+
   def median
-    if self.length == 0
-      return nil
-    end
-    if self.length.odd?
-      return self.sort[self.length/2]
-    end
-    if self.length.even?
-      a =self.sort[self.length/2]
-      b = self.sort[self.length/2-1]
-      return (a.to_f + b.to_f) / 2
-    end
+    return nil if length.zero?
+
+    sort[length / 2] + sort[(length - 1) / 2] / 2.0
   end
+
   def counts
     count = Hash.new(0)
-    self.each {|key| count[key]+=1}
-    return count
+    each { |key| count[key] += 1 }
+    count
   end
+
   def my_count(value)
-    self.counts[value]
+    counts[value]
   end
+
   def my_index(value)
-    if self.include?(value)
-      self.each_with_index do |el,i|
-        if value == el
-          return i
-        end
+    if include?(value)
+      each_with_index do |el, i|
+        return i if value == el
       end
     end
-    return nil
+    nil
   end
+
   def my_uniq
     uniqs = []
-    self.each_with_index do |el, i|
-      if !self[i+1..-1].include?(el)
-          uniqs << el
-      end
+    each_with_index do |el, i|
+      uniqs << el unless self[i + 1..-1].include?(el)
     end
-    return uniqs
+    uniqs
   end
+
   def my_transpose
-    if self.length == 0 
-      return nil
-    end
-    transposed = Array.new(self[0].length){Array.new(self.length)}
-    self.each_with_index do |row, idx1|
+    return nil if length.zero?
+
+    transposed = Array.new(self[0].length) { Array.new(length) }
+    each_with_index do |row, idx1|
       row.each_with_index do |val, idx2|
         transposed[idx2][idx1] = val
       end
