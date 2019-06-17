44da1c4ed5af50ee385a95c96150975ebee89529
Eric Amrhein <eamrhein@protonmail.ch>
2019-06-16 22:50:52 -0700

    Guessing Game Refactored

--- guessing_game_project/lib/guessing_game.rb
+++ guessing_game_project/lib/guessing_game.rb
@@ -1,32 +1,30 @@
+# frozen_string_literal: true
+
+# creates a guessing game object which attempts to guess a number
 class GuessingGame
+  attr_reader :num_attempts
   def initialize(min, max)
     @secret_num = rand(min..max)
     @num_attempts = 0
     @game_over = false
   end
-  def num_attempts
-    @num_attempts
-  end
+
   def game_over?
     @game_over
   end
+
   def check_num(num)
-    @num_attempts +=1
+    @num_attempts += 1
     if num == @secret_num
       @game_over = true
-      puts "you win"
+      puts 'you win'
     end
-    if num > @secret_num
-      puts "too big"
-    end
-    if num < @secret_num
-      puts "too small"
-    end
-  end
-  def ask_user
-    puts "enter a number"
-    input = gets.chomp.to_i
-    self.check_num(input)
+    num > @secret_num ? puts('too big') : puts('too small')
   end
 
+  def ask_user
+    puts 'enter a number'
+    input = gets.chomp.to_i
+    check_num(input)
+  end
 end
