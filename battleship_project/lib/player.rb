258c0864a2382bf5c6239da65cae069df9c4d3fb
Eric Amrhein <eamrhein@protonmail.ch>
2019-06-21 13:11:18 -0700

    Revert "BattleShip Project"
    This reverts commit ee7bdcdefe955ffa2bbceac0190390279cbc3eb1.

--- battleship_project/lib/player.rb
+++ battleship_project/lib/player.rb
@@ -1,10 +1,3 @@
-# frozen_string_literal: true
-
-# class gives instructions to the player
 class Player
-  def get_move
-    puts "enter a position with coordinates separated with a space like '4 7'"
-    arr = gets.chomp("' '")
-    arr.split.map(&:to_i)
-  end
+
 end
