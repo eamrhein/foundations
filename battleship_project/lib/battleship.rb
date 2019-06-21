2bf74ca8d8b408d38310576d84dabf81e8f519c6
Eric Amrhein <eamrhein@protonmail.ch>
2019-06-21 13:40:18 -0700

    Revert "Revert "BattleShip Project""
    This reverts commit 258c0864a2382bf5c6239da65cae069df9c4d3fb.

--- battleship_project/lib/battleship.rb
+++ battleship_project/lib/battleship.rb
@@ -1,6 +1,49 @@
-require_relative "board"
-require_relative "player"
+# frozen_string_literal: true
 
+require_relative 'board'
+require_relative 'player'
+
+# battleship game env
 class Battleship
+  attr_reader :board, :player
+  def initialize(dim)
+    @player = Player.new
+    @board = Board.new(dim)
+    @remaining_misses = @board.size / 2
+  end
 
+  def start_game
+    @board.place_random_ships
+    puts @board.num_ships.to_s + ' ships place on the board.'
+    @board.print
+  end
+
+  def lose?
+    if @remaining_misses <= 0
+      puts 'you lose'
+      true
+    else
+      false
+    end
+  end
+
+  def win?
+    if @board.num_ships.zero?
+      puts 'you win'
+      true
+    else
+      false
+    end
+  end
+
+  def game_over?
+    win? || lose?
+  end
+
+  def turn
+    move = @player.get_move
+    @remaining_misses -= 1 unless @board.attack(move)
+    @board.print
+    puts @remaining_misses
+  end
 end
