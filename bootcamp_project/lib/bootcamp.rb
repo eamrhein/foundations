53844aa367d52db33660adae10b721b0ef1c7413
Eric Amrhein <eamrhein@protonmail.ch>
2019-06-30 19:50:21 -0700

    Funky Dory

--- bootcamp_project/lib/bootcamp.rb
+++ bootcamp_project/lib/bootcamp.rb
@@ -1,56 +1,55 @@
+# frozen_string_literal: true
+
+# bootcamp class
 class Bootcamp
-def initialize(name, slogan, student_capacity)
-  @name = name
-  @slogan = slogan
-  @student_capacity = student_capacity
-  @students = []
-  @teachers = []
-  @grades = Hash.new {|hash,key| hash[key] = []}
-end
-def name
-  @name
-end
-def slogan
-  @slogan
-end
-def teachers
-  @teachers
-end
-def students
-  @students
-end
-def hire(teacher)
-  @teachers << teacher
-end
-def enroll(student)
-  if @students.length < @student_capacity
-    @students << student
-    return true
-  else
-    return false
+  atrr_reader :name, :slogan, :teachers, :students
+  def initialize(name, slogan, student_capacity)
+    @name = name
+    @slogan = slogan
+    @student_capacity = student_capacity
+    @students = []
+    @teachers = []
+    @grades = Hash.new {|hash,key| hash[key] = []}
   end
-end
-def enrolled?(student)
-  @students.map(&:downcase).include?(student.downcase)
-end
-def student_to_teacher_ratio
-  @students.length / @teachers.length
-end
-def add_grade(student, grade)
-  if self.enrolled?(student)
-    @grades[student] << grade
-    return true
-  else
-    return false
+
+  def hire(teacher)
+    @teachers << teacher
   end
-end
-def num_grades(student)
-  @grades[student].length
-end
-def average_grade(student)
-  if num_grades(student) == 0
-    return nil
+
+  def enroll(student)
+    if @students.length < @student_capacity
+      @students << student
+      return true
+    else
+      return false
+    end
   end
-  @grades[student].sum / num_grades(student)
-end
+
+  def enrolled?(student)
+    @students.map(&:downcase).include?(student.downcase)
+  end
+
+  def student_to_teacher_ratio
+    @students.length / @teachers.length
+  end
+
+  def add_grade(student, grade)
+    if self.enrolled?(student)
+      @grades[student] << grade
+      return true
+    else
+      return false
+    end
+  end
+
+  def num_grades(student)
+    @grades[student].length
+  end
+
+  def average_grade(student)
+    if num_grades(student) == 0
+      return nil
+    end
+    @grades[student].sum / num_grades(student)
+  end
 end
--- procs_project/lib/part_1.rb
+++ procs_project/lib/part_1.rb
@@ -1,28 +1,35 @@
+# frozen_string_literal: true
+
 require 'byebug'
 def my_map(arr, &prc)
   result = []
-  arr.each {|el| result <<  prc.call(el)} 
+  arr.each { |el| result << prc.call(el) }
   result
 end
+
 def my_select(arr, &prc)
   result = []
-  arr.each {|el| result << el if prc.call(el)==true}
+  arr.each { |el| result << el if prc.call(el) == true }
   result
 end
+
 def my_count(arr, &prc)
-  count=0
-  arr.each {|el| count+=1 if prc.call(el)==true}
+  count = 0
+  arr.each { |el| count += 1 if prc.call(el) == true }
   count
 end
+
 def my_any?(arr, &prc)
-  arr.each {|el| return true if prc.call(el)==true}
-  return false
+  arr.each { |el| return true if prc.call(el) == true }
+  false
 end
+
 def my_all?(arr, &prc)
-  arr.each {|el| return false if prc.call(el)==false}
-  return true
+  arr.each { |el| return false if prc.call(el) == false }
+  true
 end
+
 def my_none?(arr, &prc)
-  arr.each {|el| return false if prc.call(el)==true}
-  return true
+  arr.each { |el| return false if prc.call(el) == true }
+  true
 end
